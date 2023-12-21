2a746fc9c9e8dfd6f2e934d8fb943144821b1b15
xiaoyachong <150726549+xiaoyachong@users.noreply.github.com>
2023-12-21 14:45:07 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import gradio as gr
 import torch
 import transformers
 from transformers import AutoModelForCausalLM, AutoTokenizer
@@ -14,29 +14,58 @@ def load_data():
 
 tokenizer, model = load_data()
 
-st.write("Welcome to the Chatbot. I am still learning, please be patient")
-input = st.text_input('User:')
-if 'count' not in st.session_state:
-    st.session_state.count = 0 
-    st.session_state.chat_history_ids = None
-    st.session_state.old_response = ''
-else:
-    st.session_state.count += 1
 
 
-new_user_input_ids = tokenizer.encode(input + tokenizer.eos_token, return_tensors='pt')
 
-bot_input_ids = torch.cat([st.session_state.chat_history_ids, new_user_input_ids], dim=-1) if st.session_state.count > 1 else new_user_input_ids
+def chat(message, history):
+  history = history or []
+  new_user_input_ids = tokenizer.encode(message + tokenizer.eos_token, return_tensors='pt')
+  
+  if len(history) > 0 and len(history) < 2:
+    for i in range(0,len(history)):
+      encoded_message = tokenizer.encode(history[i][0] + tokenizer.eos_token, return_tensors='pt')
+      encoded_response = tokenizer.encode(history[i][1] + tokenizer.eos_token, return_tensors='pt')
+      if i == 0:
+        chat_history_ids = encoded_message
+        chat_history_ids = torch.cat([chat_history_ids,encoded_response], dim=-1)
+      else:
+        chat_history_ids = torch.cat([chat_history_ids,encoded_message], dim=-1)
+        chat_history_ids = torch.cat([chat_history_ids,encoded_response], dim=-1)
+      
+      bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1)
 
-st.session_state.chat_history_ids = model.generate(bot_input_ids, max_length=5000, pad_token_id=tokenizer.eos_token_id)
+  elif len(history) >= 2:
+      for i in range(len(history)-1, len(history)):
+        encoded_message = tokenizer.encode(history[i][0] + tokenizer.eos_token, return_tensors='pt')
+        encoded_response = tokenizer.encode(history[i][1] + tokenizer.eos_token, return_tensors='pt')
+        if i == (len(history)-1):
+          chat_history_ids = encoded_message
+          chat_history_ids = torch.cat([chat_history_ids,encoded_response], dim=-1)
+        else:
+          chat_history_ids = torch.cat([chat_history_ids,encoded_message], dim=-1)
+          chat_history_ids = torch.cat([chat_history_ids,encoded_response], dim=-1)
+      
+      bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1)
+  
+  elif len(history) == 0:
+    bot_input_ids =  new_user_input_ids
 
-response = tokenizer.decode(st.session_state.chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
-
-if st.session_state.old_response == response:
-    bot_input_ids = new_user_input_ids
+  chat_history_ids = model.generate(bot_input_ids, max_length=1000, do_sample=True, top_p=0.9, temperature=0.8, pad_token_id=tokenizer.eos_token_id)
+  response = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
     
-    st.session_state.chat_history_ids = model.generate(bot_input_ids, max_length=5000, pad_token_id=tokenizer.eos_token_id)
-    response = tokenizer.decode(st.session_state.chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+  history.append((message, response))
 
-st.write(f"Chatbot: {response}")
-st.session_state.old_response = response
+  return history, history
+
+title = "DialoGPT fine-tuned on DailyDialog"
+description = "Gradio demo for open-domain dialog using DialoGPT. Model was fine-tuned on the DailyDialog multi-turn dataset"
+iface = gr.Interface(
+    chat,
+    ["text", "state"],
+    ["chatbot", "state"],
+    allow_screenshot=False,
+    allow_flagging="never",
+    title=title,
+    description=description
+)
+iface.launch(debug=True)
